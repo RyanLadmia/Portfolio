5145980cf4cc414bdca8ac6d9a94a18d72c880c8
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-08-23 10:47:40 +0200

    dossiers

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../includes/_head.php'; ?>
+
+<body>
+    <?php include '../includes/_header.php'; ?>
+    <main>
+
+    </main>
+    <?php include '../includes/_footer.php'; ?>
+</body>
+</html>
