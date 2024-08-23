aff95e3e1453192a736ca5b5a070114de89510ed
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-08-23 14:52:09 +0200

    d

--- includes/_head.php
+++ includes/_head.php
@@ -2,6 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Assets/CSS/index.css">
-    <link class="favicon" rel="icon" type="image/x-icon" href="Assets/medias/pictures/favicon.png">
+    <link class="favicon" rel="icon" type="image/x-icon" href="Assets/medias/pictures/dev.png">
     <title>Portfolio</title>
 </head>
